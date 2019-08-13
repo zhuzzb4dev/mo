54be9037a1e4f40e508182f973f90a38819bf82e
zhuzhb@10000e.com.cn <zhu7595842zhu>
2019-08-13 16:12:14 +0800

    Initial commit

---
+++ moserver/src/main/java/ga/zhuzimo/moserver/MoserverApplication.java
@@ -0,0 +1,13 @@
+package ga.zhuzimo.moserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoserverApplication.class, args);
+    }
+
+}
